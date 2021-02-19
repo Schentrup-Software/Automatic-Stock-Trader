22ce3d721caa211954c312c9e52c66894cc25212
Joseph Schentrup <j03y@live.com>
2021-02-18 21:15:04 -0500

    Push up CSV file builder

---
+++ AutomaticStockTrader.Tests/Tracking/TrackingRecordWriter.cs
@@ -0,0 +1,38 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AutomaticStockTrader.Tests.Tracking
+{
+    public static class TrackingRecordWriter
+    {
+        private const string FILE_PATH = "tracking.csv";
+
+        public static void WriteData(TrackingRecord record)
+        {
+            if (File.Exists(FILE_PATH))
+            {
+                using var stream = File.Open(FILE_PATH, FileMode.Append);
+                using var writer = new StreamWriter(stream);
+                using var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    HasHeaderRecord = false
+                });
+
+                csv.WriteRecords(new List<TrackingRecord> { record });
+                writer.Flush();
+            }
+            else
+            {
+                using var writer = new StreamWriter(FILE_PATH);
+                using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+                csv.WriteRecords(new List<TrackingRecord> { record });
+                writer.Flush();
+            }
+        }
+    }
+}
