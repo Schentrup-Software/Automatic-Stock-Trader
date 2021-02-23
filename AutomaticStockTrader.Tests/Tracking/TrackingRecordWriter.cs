09264bfe4b1c040c904bebf85d240f630573dd67
Joey Schentrup <j03y@live.com>
2021-02-23 18:37:13 -0500

    More large test debugging

--- AutomaticStockTrader.Tests/Tracking/TrackingRecordWriter.cs
+++ AutomaticStockTrader.Tests/Tracking/TrackingRecordWriter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Google.Cloud.Firestore;
 
@@ -10,6 +11,7 @@ namespace AutomaticStockTrader.Tests.Tracking
 
         public static async Task WriteData(TrackingConfig config, double percentageMade, string strategyName)
         {
+            Debug.Write($"Google app creds: {config.Google_Application_Credentials}");
             if (!string.IsNullOrWhiteSpace(config.Google_Application_Credentials))
             {
                 await (await FirestoreDb.CreateAsync("automatic-stock-trader-tracker")).Collection(COLLECTION).AddAsync(new { DateTime.UtcNow.Date, percentageMade, strategyName });
